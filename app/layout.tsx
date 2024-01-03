0b99a2aecf4ff2616d96c14d7f4b251f2161b937
mateuszNadolny <mateusznadolny00@gmail.com>
2024-01-03 17:19:27 +0100

    created auth pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,21 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import { ClerkProvider } from '@clerk/nextjs';
+import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Devalliance',
-  description: 'Discover and contribute to coding projects',
-}
+  title: 'DevAlliance',
+  description: 'Discover and contribute to coding projects'
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
 }
